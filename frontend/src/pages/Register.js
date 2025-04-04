9b0dd63bac0f90e7b4573142100343ae1db224de
Avinash Elavarasu <112918945+avinxsh66@users.noreply.github.com>
2025-04-04 09:53:19 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/register", {
+      await axios.post("https://e-commerce-backend-tbvg.onrender.com/api/auth/register", {
         name,
         email,
         password,
