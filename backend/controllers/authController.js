af3549b4121a53df4b37c854118ba86d2f889979
Avinash Elavarasu <112918945+avinxsh66@users.noreply.github.com>
2025-04-04 10:27:03 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -49,10 +49,12 @@ export const login = async (req, res) => {
       expiresIn: "7d",
     });
 
+    // Set token in cookie
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
+      domain: "e-commerce-client-gvay.onrender.com",
     });
 
     res.status(200).json({
@@ -66,7 +68,13 @@ export const login = async (req, res) => {
 
 // Logout user
 export const logout = (req, res) => {
-  res.cookie("token", "", { httpOnly: true, expires: new Date(0) });
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
+    domain: "e-commerce-client-gvay.onrender.com",
+  });
   res.status(200).json({ message: "Logged out successfully!" });
 };
 
